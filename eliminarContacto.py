1eea7c126cd87af73abd91e8924bb453977180ab
Jose Maria <josemariagarcia95@gmail.com>
2016-03-15 17:51:44 +0100

    Creacion Repositorio. Practica 5 casi terminad

---
+++ eliminarContacto.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# coding: utf-8
+#
+import xmlrpclib
+import sys
+# Datos de conexión (adaptar a cada caso)
+#
+server = "localhost"
+port = 8069
+dbname = "bd2"
+user_name = "admin"
+user_passwd= "postgres"
+
+if len(sys.argv) != 2 :
+    raise StandardError("Faltan argumentos o hay de mas")
+nombreEmpleado = sys.argv[1]
+conn = xmlrpclib.ServerProxy('http://%s:%s/xmlrpc/2/%s' % (server, port, 'common'))
+uid = conn.authenticate(dbname, user_name, user_passwd, {})
+object = xmlrpclib.ServerProxy('http://%s:%s/xmlrpc/2/%s' % (server, port, 'object'))
+
+idContacto = {}
+idContacto = object.execute_kw(dbname, uid, user_passwd, 'res.partner', 'search_read',
+                             [[['name','=',nombreEmpleado]]],{'fields':['name']})
+if len(idContacto) != 1 :
+    raise Exception("Deberia haber una persona con ese nombre")
+
+print idContacto[0]['id']
+sino = raw_input("Esta seguro de que desea borrar a ese contacto? \n")
+if sino in ('Si','si','S'):
+    object.execute_kw(dbname, uid, user_passwd, 'res.partner', 'unlink', [[idContacto[0]['id']]])
+    compania = object.execute_kw(dbname, uid, user_passwd, 'res.partner', 'search_read', [[['id','=',idContacto[0]['id']]]])
+    print('Partners con el id' + int(idContacto[0]['id']) + ' : ' + compania)
--- nuevaCompaniaCliente.py
+++ nuevaCompaniaCliente.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 #
 import xmlrpclib
-
+import sys
 # Datos de conexión (adaptar a cada caso)
 #
 server = "localhost"
@@ -11,7 +11,36 @@ dbname = "bd2"
 user_name = "admin"
 user_passwd= "postgres"
 
-
+if len(sys.argv) != 2 :
+    raise StandardError("Faltan argumentos o hay de mas")
+nombreEmpresa = sys.argv[1]
 conn = xmlrpclib.ServerProxy('http://%s:%s/xmlrpc/2/%s' % (server, port, 'common'))
 uid = conn.authenticate(dbname, user_name, user_passwd, {})
 object = xmlrpclib.ServerProxy('http://%s:%s/xmlrpc/2/%s' % (server, port, 'object'))
+
+compania = {}
+compania = object.execute_kw(dbname, uid, user_passwd, 'res.partner', 'search_read',
+                             [[['is_company', '=' ,True],['name','=',nombreEmpresa]]],{'fields':['name']})
+if len(compania) != 0 :
+    raise Exception("Ya existe una empresa con ese nombre")
+idCompania = object.execute_kw(dbname, uid, user_passwd, 'res.partner', 'create', [{'name': nombreEmpresa,'is_company':True}])
+print 'El id de la nueva compania es {0}'.format(idCompania)
+
+nombreContacto = raw_input("Introduzca nombre \n")
+calleContacto = raw_input("Introduzca calle \n")
+ciudadContacto = raw_input("Introduzca ciudad \n")
+telefonoContacto = raw_input("Introduzca telefono \n")
+faxContacto = raw_input("Introduzca fax \n")
+emailContacto = raw_input("Introduzca email \n")
+idContacto = object.execute_kw(dbname, uid, user_passwd, 'res.partner', 'create', [{'name': nombreContacto,
+                                                                                    'street':calleContacto,
+                                                                                    'email':emailContacto,
+                                                                                    'fax':faxContacto,
+                                                                                    'phone':telefonoContacto,
+                                                                                    'parent_id':idCompania,
+                                                                                    'city':ciudadContacto,
+                                                                                    'type':'contact'
+                                                                                     }])
+print('El id del contacto es {0}'.format(idContacto))
+compania = object.execute_kw(dbname, uid, user_passwd, 'res.partner', 'search_read', [[['is_company', '=' ,True],['id','=',idCompania]]],{'fields':['child_ids']})
+print('Los ids de los contactos de la empresa son \n',compania)
