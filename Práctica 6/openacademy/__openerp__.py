18e91b44a25945c558df0b462113605fd104aa64
Jose Maria <josemariagarcia95@gmail.com>
2016-04-15 23:55:39 +0200

    Codigo del modulo de la practica 6

---
+++ Práctica 6/openacademy/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """Manage trainings""",
+
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "Jose Maria S.L.",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+        'views/openacademy.xml',
+        'demo.xml',
+        'views/partner.xml',
+        'reports.xml',
+    ],
+    # only loaded in demonstration mode (cuando pedimos que cree una base con datos de demostracion)
+    'demo': [],
+}
